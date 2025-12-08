12ac552dbb4ba440c3533771708b1ca78b569764
Hal34329 <jesusguillen545@gmail.com>
2025-12-07 23:37:05 -0600

    First endpoint

---
+++ build/index.js
@@ -0,0 +1,11 @@
+import express from "express";
+const app = express();
+app.use(express.json()); // Middleware que transforma la red.body a un JSON
+const PORT = 3000;
+app.get("/ping", (_req, res) => {
+    console.log("Someone pinged here!!");
+    res.send("pong");
+});
+app.listen(PORT, () => {
+    console.log("Server running on port ${PORT}");
+});
