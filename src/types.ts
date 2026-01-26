508d461c9c85d271259c6d7b15fcfd50306f17f8
Hal34329 <jesusguillen545@gmail.com>
2026-01-26 00:02:21 -0600

    Change Types to Zod syntax

--- src/types.ts
+++ src/types.ts
@@ -1,16 +1,36 @@
+// export const WeatherValues = ["sunny", "rainy", "cloudy", "windy", "stormy"] as const;
+// export type Weather = typeof WeatherValues[number];
+
+// export const VisibilityValues = ["great", "good", "ok", "poor"] as const;
+// export type Visibility = typeof VisibilityValues[number];
+
+// export interface DiaryEntry {
+//     id: number,
+//     date: string,
+//     weather: Weather,
+//     visibility: Visibility,
+//     comment: string
+// }
+
+// export type NonSensitiveInfoDiaryEntry = Omit<DiaryEntry, "comment">;
+// export type NewDiaryEntry = Omit<DiaryEntry, "id">;
+
+import { z } from "zod";
+
 export const WeatherValues = ["sunny", "rainy", "cloudy", "windy", "stormy"] as const;
-export type Weather = typeof WeatherValues[number];
-
 export const VisibilityValues = ["great", "good", "ok", "poor"] as const;
-export type Visibility = typeof VisibilityValues[number];
 
-export interface DiaryEntry {
-    id: number,
-    date: string,
-    weather: Weather,
-    visibility: Visibility,
-    comment: string
-}
+export const diarySchema = z.object({
+    id: z.number(),
+    date: z.iso.date(),
+    weather: z.enum(WeatherValues),
+    visibility: z.enum(VisibilityValues),
+    comment: z.string(),
+});
 
-export type NonSensitiveInfoDiaryEntry = Omit<DiaryEntry, "comment">;
-export type NewDiaryEntry = Omit<DiaryEntry, "id">;
+export const newDiarySchema = diarySchema.omit({ id: true });
+export const NonSensitiveInfoDiaryEntrySchema = diarySchema.omit({ comment: true });
+
+export type DiaryEntry = z.infer<typeof diarySchema >;
+export type NonSensitiveInfoDiaryEntry = z.infer<typeof NonSensitiveInfoDiaryEntrySchema>;
+export type NewDiaryEntry = z.infer<typeof newDiarySchema>;
