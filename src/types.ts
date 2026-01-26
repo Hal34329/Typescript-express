847fbc9693b9991e638123687bf9dc258715b37d
Hal34329 <jesusguillen545@gmail.com>
2026-01-26 00:08:13 -0600

    Remove comments

--- src/types.ts
+++ src/types.ts
@@ -1,20 +1,3 @@
-// export const WeatherValues = ["sunny", "rainy", "cloudy", "windy", "stormy"] as const;
-// export type Weather = typeof WeatherValues[number];
-
-// export const VisibilityValues = ["great", "good", "ok", "poor"] as const;
-// export type Visibility = typeof VisibilityValues[number];
-
-// export interface DiaryEntry {
-//     id: number,
-//     date: string,
-//     weather: Weather,
-//     visibility: Visibility,
-//     comment: string
-// }
-
-// export type NonSensitiveInfoDiaryEntry = Omit<DiaryEntry, "comment">;
-// export type NewDiaryEntry = Omit<DiaryEntry, "id">;
-
 import { z } from "zod";
 
 export const WeatherValues = ["sunny", "rainy", "cloudy", "windy", "stormy"] as const;
