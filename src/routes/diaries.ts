c55e8cd90a90f2940638b169a045ca7846fc4342
Hal34329 <jesusguillen545@gmail.com>
2026-01-27 19:27:36 -0600

    Removed comments

--- src/routes/diaries.ts
+++ src/routes/diaries.ts
@@ -4,65 +4,42 @@ import toNewDiaryEntry from "../utils.js";
 
 const router = express.Router();
 
-router.get("/", (_req, res) => {
-    res.send(diaryServices.getEntriesWithoutSensitiveInfo());
+// router.get("/", (_req, res) => {
+//     res.send(diaryServices.getEntriesWithoutSensitiveInfo());
+// });
+
+router.get("/", async (_req, res) => {
+    const entries = await diaryServices.getEntriesWithoutSensitiveInfo();
+    res.json(entries);
 });
 
 router.get("/full", async (_req, res) => {
     try{
-        // res.send(diaryServices.getEntries());
         const entries = await diaryServices.getEntries();
         res.json(entries);
     } catch (e: unknown) {
-        // res.status(500).send("Error al obtener las entradas");
-        // if(e instanceof Error){
-        //     res.status(400).send(`Error: ${e.message}`);
-        // }
-        // else {
-        //     res.status(400).send("Unknown error");
-        // }
         const errorMessage = e instanceof Error ? e.message : "Unknown Error";
         res.status(500).json({ error: errorMessage });
     }
 });
 
-// router.get("/:id", (req, res) => {
-//     const diary = diaryServices.findById(Number(req.params.id));
-//     return (diary !== null)
-//         ? res.send(diary)
-//         : res.sendStatus(404);
-// });
-
 router.get("/:id", async (req, res) => {
-    const diary = diaryServices.findById(Number(req.params.id));
+    const diary = await diaryServices.findById(Number(req.params.id));
     return (diary!==null) ? res.send(diary) : res.sendStatus(404);
 });
-
-// router.post("/", (req, res) => {
-//     try {
-//         const newDiaryEntry = toNewDiaryEntry(req.body);
-
-//         const addedDiaryEntry = diaryServices.addDiary(newDiaryEntry);
-
-//         res.json(addedDiaryEntry);
-//     } catch (e) {
-//         if(e instanceof Error){
-//             res.status(400).send(e.message);
-//             // res.status(400).send((e as Error).message);
-//         }
-//         else {
-//             res.status(400).send("Unknown error");
-//         }
-//     }
-// });
 
 router.post("/", async (req, res) => {
     try {
         const newDiaryEntry = toNewDiaryEntry(req.body);
-        const addedDiaryEntry = diaryServices.addDiary(newDiaryEntry);
+        const addedDiaryEntry = await diaryServices.addDiary(newDiaryEntry);
         res.json(addedDiaryEntry);
     } catch (e: unknown) {
-        const errorMessage = e instanceof Error ? e.message : "Unknown Error";
+        let errorMessage;
+        try {
+            errorMessage = e instanceof Error ? JSON.parse(e.message) : "Unknown Error";
+        } catch {
+            errorMessage = e instanceof Error ? e.message : "Unknown Error";
+        }
         res.status(400).send({ error:errorMessage });
     }
 });
