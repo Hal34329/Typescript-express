d8d960f77c9b82a6e13018b19d42bc34662ddea0
Hal34329 <jesusguillen545@gmail.com>
2026-01-27 19:01:10 -0600

    Migrate from JSON to Drizzle

--- src/services/diaryServices.ts
+++ src/services/diaryServices.ts
@@ -2,20 +2,19 @@ import type { DiaryEntry, NonSensitiveInfoDiaryEntry, NewDiaryEntry } from "@typ
 // import diaryData from "./diaries.json" with { type: "json" };
 import { db } from "../db/db.js";
 import { diaryEntries } from "../db/schema.js";
+import { eq } from "drizzle-orm";
 
 // const diaries: DiaryEntry[] = diaryData as DiaryEntry[];
 
 // export const getEntries = (): DiaryEntry[] => diaries;
 export const getEntries = async (): Promise<DiaryEntry[]> => {
-    const rows = await db.select({ 
+    return await db.select({ 
         id: diaryEntries.id, 
         date: diaryEntries.date, 
         weather: diaryEntries.weather, 
         visibility: diaryEntries.visibility, 
         comment: diaryEntries.comment, 
     }).from(diaryEntries);
-
-    return rows as DiaryEntry[];
 };
     
 // export const findById = (id: number): NonSensitiveInfoDiaryEntry | undefined => {
@@ -26,6 +25,17 @@ export const getEntries = async (): Promise<DiaryEntry[]> => {
 //     }
 //     return undefined;
 // };
+
+export const findById = async (id: number): Promise<NonSensitiveInfoDiaryEntry | undefined> => {
+    const [entry] = await db.select({
+        id: diaryEntries.id,
+        date: diaryEntries.date,
+        weather: diaryEntries.weather,
+        visibility: diaryEntries.visibility,
+    }).from(diaryEntries).where(eq(diaryEntries.id, id)).limit(1);
+
+    return entry;
+};
 
 // export const getEntriesWithoutSensitiveInfo = (): NonSensitiveInfoDiaryEntry[] => {
 //     return diaries.map(({ id, date, weather, visibility }) => {
@@ -38,6 +48,15 @@ export const getEntries = async (): Promise<DiaryEntry[]> => {
 //     });
 // };
 
+export const getEntriesWithoutSensitiveInfo = async (): Promise<NonSensitiveInfoDiaryEntry[]> => {
+    return await db.select({
+        id: diaryEntries.id,
+        date: diaryEntries.date,
+        weather: diaryEntries.weather,
+        visibility: diaryEntries.visibility,
+    }).from(diaryEntries);
+};
+
 // export const addDiary = (newDiaryEntry: NewDiaryEntry): DiaryEntry => {
 //     const newDiary = {
 //         id: Math.max(...diaries.map(d => d.id)) + 1,
@@ -47,3 +66,13 @@ export const getEntries = async (): Promise<DiaryEntry[]> => {
 //     diaries.push(newDiary);
 //     return newDiary;
 // };
+
+export const addDiary = async (newDiaryEntry: NewDiaryEntry): Promise<DiaryEntry> => {
+    // return await db.insert(diaryEntries).values([
+    //     // ID generado por Postgres
+    //     entry,
+    // ]);
+    const [newEntry] = await db.insert(diaryEntries).values(newDiaryEntry).returning();
+
+    return newEntry!;
+};
