9868b280b5dd098b91a92d5d4c5358d9bfe44af9
Hal34329 <jesusguillen545@gmail.com>
2026-01-29 11:40:01 -0600

    Remove comments

--- src/types.ts
+++ src/types.ts
@@ -1,24 +1,3 @@
-// import { z } from "zod";
-
-// export const WeatherValues = ["sunny", "rainy", "cloudy", "windy", "stormy"] as const;
-// export const VisibilityValues = ["great", "good", "ok", "poor"] as const;
-
-// export const diarySchema = z.object({
-//     id: z.number(),
-//     date: z.iso.date(),
-//     weather: z.enum(WeatherValues),
-//     visibility: z.enum(VisibilityValues),
-//     comment: z.string(),
-// });
-
-// export const newDiarySchema = diarySchema.omit({ id: true });
-// export const partialDiaryEntrySchema = newDiarySchema.partial();
-// export const NonSensitiveInfoDiaryEntrySchema = diarySchema.omit({ comment: true });
-
-// export type DiaryEntry = z.infer<typeof diarySchema >;
-// export type NonSensitiveInfoDiaryEntry = z.infer<typeof NonSensitiveInfoDiaryEntrySchema>;
-// export type NewDiaryEntry = z.infer<typeof newDiarySchema>;
-
 import { createInsertSchema, createSelectSchema } from "drizzle-zod";
 import { diaryEntries } from "./db/schema.js";
 import { z } from "zod";
