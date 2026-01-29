c47bb210d6a0960b7621abb841d5dce9d508de2b
Hal34329 <jesusguillen545@gmail.com>
2026-01-29 11:38:43 -0600

    Feat: Add drizzle-zod and made SSOT of types

--- src/types.ts
+++ src/types.ts
@@ -1,20 +1,39 @@
+// import { z } from "zod";
+
+// export const WeatherValues = ["sunny", "rainy", "cloudy", "windy", "stormy"] as const;
+// export const VisibilityValues = ["great", "good", "ok", "poor"] as const;
+
+// export const diarySchema = z.object({
+//     id: z.number(),
+//     date: z.iso.date(),
+//     weather: z.enum(WeatherValues),
+//     visibility: z.enum(VisibilityValues),
+//     comment: z.string(),
+// });
+
+// export const newDiarySchema = diarySchema.omit({ id: true });
+// export const partialDiaryEntrySchema = newDiarySchema.partial();
+// export const NonSensitiveInfoDiaryEntrySchema = diarySchema.omit({ comment: true });
+
+// export type DiaryEntry = z.infer<typeof diarySchema >;
+// export type NonSensitiveInfoDiaryEntry = z.infer<typeof NonSensitiveInfoDiaryEntrySchema>;
+// export type NewDiaryEntry = z.infer<typeof newDiarySchema>;
+
+import { createInsertSchema, createSelectSchema } from "drizzle-zod";
+import { diaryEntries } from "./db/schema.js";
 import { z } from "zod";
 
 export const WeatherValues = ["sunny", "rainy", "cloudy", "windy", "stormy"] as const;
 export const VisibilityValues = ["great", "good", "ok", "poor"] as const;
 
-export const diarySchema = z.object({
-    id: z.number(),
+export const diarySchema = createSelectSchema(diaryEntries, {
     date: z.iso.date(),
-    weather: z.enum(WeatherValues),
-    visibility: z.enum(VisibilityValues),
-    comment: z.string(),
 });
 
-export const partialDiaryEntrySchema = diarySchema.partial().omit({ id:true });
-export const newDiarySchema = diarySchema.omit({ id: true });
-export const NonSensitiveInfoDiaryEntrySchema = diarySchema.omit({ comment: true });
+export const newDiarySchema = createInsertSchema(diaryEntries).omit({ id: true });
+export const partialDiaryEntrySchema = newDiarySchema.partial();
+export const NonSensitiveInfoDiaryEntrySchema = diarySchema.omit({ comment:true });
 
-export type DiaryEntry = z.infer<typeof diarySchema >;
-export type NonSensitiveInfoDiaryEntry = z.infer<typeof NonSensitiveInfoDiaryEntrySchema>;
+export type DiaryEntry = z.infer<typeof diarySchema>;
 export type NewDiaryEntry = z.infer<typeof newDiarySchema>;
+export type NonSensitiveInfoDiaryEntry = z.infer<typeof NonSensitiveInfoDiaryEntrySchema>;
